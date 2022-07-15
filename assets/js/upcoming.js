d210e081c18c908dc83db4dd87a8a2821b11ed61
GiorgiMats <giorgimatsukatov@gmail.com>
2022-07-15 22:07:35 +0400

    bolo

--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -1,3 +1,7 @@
+let picArray = ['1', '2', '3', '4'];
+let currpicnum = 0;
+
+
 function changepicture(picture) {
     let pic = document.querySelector('#upcoming-main-image-inside');
     let smallpic = document.querySelector(`#small-image-${picture}`);
@@ -5,4 +9,24 @@ function changepicture(picture) {
     pic.classList.add(`main-image-${picture}`);
     document.querySelector('.active-small-image').classList.remove('active-small-image');
     smallpic.classList.add('active-small-image');
+}
+
+function nextpic() {
+    if(currpicnum == 3) {
+        currpicnum = 0;
+        changepicture(picArray[currpicnum]);
+    } else {
+        changepicture(picArray[currpicnum+1]);
+        currpicnum++;
+    }
+}
+
+function prevpic() {
+    if(currpicnum == 0) {
+        currpicnum = 3;
+        changepicture(picArray[currpicnum]);
+    } else {
+        changepicture(picArray[currpicnum-1]);
+        currpicnum--;
+    }
 }
